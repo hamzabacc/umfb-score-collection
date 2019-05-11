dd40e511e4e8d251c04812fcd0fb0575392ff0be
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-05-11 12:34:04 -0400

    accessing full html text, sorting through tags

--- scorescrape.py
+++ scorescrape.py
@@ -20,28 +20,37 @@ def getFootballGames(url):
     #driver = webdriver.Chrome(executable_path=r"C:\Chrome\chromedriver.exe")
     driver = webdriver.Chrome(executable_path = path)
     driver.get(url)
+
     #print(driver)
-    print(driver.find_element_by_link_text('football'))
-    sys.exit()
+    #print(driver.find_element_by_link_text('football'))
+    #sys.exit()
     #url = input('Enter - ')
     #html = urlopen(url).read()
-    html=requests.get(url)
-    soup = BeautifulSoup(html.text, "html.parser")
+    html=driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
     #print(soup)
     #info= soup.find_all('div', class_="ReactVirtualized__Grid__innerScrollContainer")
     #print(info)
     #print(soup.get_text())
     b=soup.find('body')
-    print(b)
-    outer = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
-    print(outer)
+    #print(b)
+    test_outer = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
+    #print(outer)
     #time.sleep(10)
 
-    outer2 = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
-    print(outer2)
+
+
+
+    outer=soup.find('div', attrs={'class':'rmq-1c61845d'})
+    #print(outer)  
+    scroll_container=soup.find('div', attrs={'class':'ReactVirtualized__Grid__innerScrollContainer'})
+    print(scroll_container)
+
+    #outer2 = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
+    #print(outer2)
     div_container=soup.find('div', class_="jss1")
     #"ReactVirtualized__Grid__innerScrollContainer" )
-    print(div_container)
+    #print(div_container)
     #outer = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
     test=soup.find_all('div')
     #for link in test:
@@ -61,7 +70,7 @@ def getFootballGames(url):
     #'class':"ReactVirtualized__Grid__innerScrollContainer"})
     #, 'role':'rowgroup','style':'width: auto; height: 2350px; max-width: 700px; max-height: 2350px; overflow: hidden; position: relative;'})
     # 'class':"ReactVirtualized__Grid__innerScrollContainer", 'role':"rowgroup", 'style':"width: auto; height: 2330px; max-width: 796px; max-height: 2330px; overflow: hidden; position: relative;"
-# Retrieve all of the anchor tags
+    # Retrieve all of the anchor tags
     tbody = soup('div')
     return inner
     val=0
@@ -73,3 +82,4 @@ def getFootballGames(url):
 print(getFootballGames('https://scorestream.com/team/pioneer-high-school-pioneers-8385/games'))
 
 #<div 'style':"overflow: visible; width: 0px;"
+
