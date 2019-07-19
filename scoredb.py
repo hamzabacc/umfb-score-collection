bb1f76b41683d2177ec5a41e93c746bedde6009f
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-07-18 22:31:20 -0400

    added sql commands, file created but data not loading yet

--- scoredb.py
+++ scoredb.py
@@ -1,21 +1,29 @@
 import sqlite3
 
 conn = sqlite3.connect('Scores.sqlite')
-cur = conn.cursor
+cur = conn.cursor()
+cur.execute('drop table if exists Schools')
+cur.execute('create table Schools(name text, scorestreamURL text)')
 
 
-
-f = file.open('DB_TEST_2020PROSPECT_18JULY2019.csv', 'r')
+f = open('DB_TEST_2020PROSPECT_18JULY2019.csv', 'r')
 lines = f.readlines()
 count = 0
 
 
 for line in lines:
+    #print(line)
     if count==0:
         count+=1
-        continue
     else:
-        name = line.split()[0]
-        url = line.split()[1]
+        name = line.split(',')[0]
+        if (line.split(',')[1]!='\n'):
+            url = line.split(',')[1]
+            command = 'insert into Schools (name, scorestreamURL) values (?,?)'
+            values = (name, url)
+            print(values)
+            cur.execute(command, values)
+
+        else: pass
 
 
