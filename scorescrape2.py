e785b4105c61e757a05228114acd4e122fb2b9c3
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-08-17 15:37:04 -0400

    making second version file to simplify data acquisition

---
+++ scorescrape2.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import ssl
+import unittest
+import requests
+import sys as sys
+import time
+import os
+import re
