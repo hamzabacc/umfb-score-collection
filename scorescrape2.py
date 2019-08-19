cf54cbb2623f1d247c47d54ed07823e577df656b
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-08-19 11:35:22 -0400

    sifting through spans to get clean school names, score aquisition is working with some faults

--- scorescrape2.py
+++ scorescrape2.py
@@ -10,6 +10,7 @@ import time
 import os
 import re
 
+
 def getScoreStream(url, sportKey=""):
     path=r'C:\Users\Hamza\Documents\Football\chromedriver.exe'
     driver = webdriver.Chrome(executable_path = path)
@@ -26,24 +27,47 @@ def getScoreStream(url, sportKey=""):
 
     scroll_container=soup.find('div', attrs={'class':'ReactVirtualized__Grid__innerScrollContainer'})
     a_tags=scroll_container.find_all('a')
+    football_game=False
     for a in a_tags:
         spans=a.find_all('span')
+        for span in spans:
+            if (span.text=='Boys Varsity Baseball'):
+                football_game=True
         div1=a.find('div')
         div2=div1.find('div')
         score_container=div2.find_all('div')[1]
-        print((a))
-        break
-    #print(len(a_tags))
+        if (football_game):
+            break
+    if (not football_game):
+        return "No score available for the selected sport,"
+    inners= div2.find_all('div',attrs={'style':"font-size: 48px; display: flex; position: relative; flex-direction: column;"})
+    ints=[]
+    final = False
+    for tag in inners[0].find_all('div'):
+        #print(tag.text)
+        try:
+            ints.append(int(tag.text))
+        except:
+            pass
+        #try:
+        if(tag.text==('Final')):
+                final=True
+        #except:pass
+    if(not final):
+        return("score available is not a final score,")
+    if(len(ints)>2):
+        return("more than two score numbers pulled. check manually,")
+    else:
+        away_score=ints[0]
+        home_score=ints[1]
     away_team=(spans[0].text+" "+spans[1].text)
     home_team=(spans[2].text+" "+spans[3].text)
     date=(spans[7].text)
-    #print(div2.text)
-
-    #for div in score_container:#.find_all('div'):
-    #    print(div.text)
-    #print(len(score_container.find_all('div')))
+    for span in spans: 
+        print(span.text)
+    return (home_team+','+str(home_score)+' to '+str(away_score)+','+away_team+','+date)
 
     
 
 
-getScoreStream('https://scorestream.com/team/pioneer-high-school-pioneers-8385/games')
+print(getScoreStream('https://scorestream.com/team/pioneer-high-school-pioneers-8385/games'))
