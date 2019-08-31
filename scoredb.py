e25da05fb04d5f8a9fbed3c5221f65be8945b04c
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-08-31 12:40:46 -0400

    code/var cleanup

--- scoredb.py
+++ scoredb.py
@@ -8,14 +8,13 @@ cur.execute('drop table if exists Schools')
 cur.execute('create table Schools(name text, scorestreamURL text)')
 
 
-f = open('DB_TEST_2020PROSPECT_18JULY2019.csv', 'r')
+f = open('DB_TEST_2020PROSPECT_27AUGUST2019.csv', 'r')
 lines = f.readlines()
 count = 0
 f.close()
 
 
 for line in lines:
-    #print(line)
     if count==0:
         count+=1
     else:
@@ -24,7 +23,6 @@ for line in lines:
             url = line.split(',')[1]
             command = 'insert into Schools (name, scorestreamURL) values (?,?)'
             values = (name, url)
-            #print(values)
             cur.execute(command, values)
 
         else: pass
@@ -33,9 +31,7 @@ conn.commit()
 
 
 f=open('newtestdoc2.csv','w')
-#f.write(scorescrape.getScoreStream)
 
-#f=open('TEST_BASEBALL_SCORES_19AUGUST2019.csv','w')
 (cur.execute('select * from Schools'))
 count=0
 f.write('team, state, opponent, score, date\n')
@@ -43,8 +39,10 @@ for row in (cur.fetchall()):
     if(len(row[1])==0):
         f.write("no scorestream link for this school")
     else:
-        f.write(scorescrape2.getScoreStream(row[1]+'/games','Boys Varsity')+"\n")
+        f.write(scorescrape2.getScoreStream(row[1]+'/games','Boys Varsity Football')+"\n")
 f.close()
+
+
 '''
 
     #if count>0:
@@ -63,20 +61,7 @@ f.close()
     schoolData.append('\n')
     f.write(','.join(schoolData))
 f.close()'''
-
-
-
-'''NEXT STEPS:
-- FORMAT MAXPREPS OUTPUT TO MATCH SCORESCRAPE OUTPUT
-- ADD CODE TO MAKE DATABASE ENTRY FOR MAXPREPS SCORES/OUTPUTS
-- CHANGE DATABASE COLUMN TITLES TO REFLECT SCORESTREAM-SPECIFIC DATA TO BE COMP'D WITH MAXPREPS DATA
-
-
-
-
-** MODIFY THE GETFOOTBALLGAMES METHOD TO ACCEPT PARAMETER OF WHICH SPORT TO RETRIEVE SCORE OF
-
-
+'''
 
 $$ ADD DATE EXTRACTION WITH SCORES AND METHOD TO CONVERT DATE SO IT ONLY RETRIEVES WITHIN LIMITED DATE PARAMETER.
 THIS WILL ALLOW SEPARATE SQL SHEET FOR EACH WEEK OF THE FOOTBALL SEASON
