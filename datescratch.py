af016b58f78b4ca3f8b3edaafd53616e7c5fe844
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-09-03 08:21:34 -0400

    code cleanup

--- datescratch.py
+++ datescratch.py
@@ -6,15 +6,7 @@
 
 from datetime import datetime, timedelta
 
-'2015-05-26'
-'''
 
-from datetime import date
-today = date.today()
-# dd/mm/YY
-d1 = today.strftime("%d/%m/%Y")
-print("d1 =", d1)
-'''
 
 #print(weekday_int)
 #print(datetime.strftime(datetime.now(), '%m/%d'))
