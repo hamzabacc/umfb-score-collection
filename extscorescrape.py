bcd2a9db7ee0e49720d57e63f3f5e59d2cf2b865
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-09-04 09:02:35 -0400

    removed old scorescrape code and replaced with updated version

--- extscorescrape.py
+++ extscorescrape.py
@@ -9,70 +9,124 @@ import sys as sys
 import time
 import os
 import re
-
-#path=r'C:\\Users\\Hamza\\Documents\\Football'
-#driver = webdriver.Chrome(executable_path = path)
-#path=r'C:\Users\Hamza\Documents\Football\chromedriver.exe'
-#driver = webdriver.Chrome(executable_path = path)
-''' ^MAKE DRIVER AND PATH UNIVERSAL VARIABLES FOR USE ACROSS FXNS'''
-#NO LONGER UNIVERSAL VARS FOR SAKE OF REUSING CHROME WEBDRIVER
-
-EXECUTION_COUNT=0
+import datescratch
 
 
-def getScoreStream(url, sportKey):
-    one_game=False
-    #football_found = False
+def getScoreStream(url, sportKey=""):
     path=r'C:\Users\Hamza\Documents\Football\chromedriver.exe'
     driver = webdriver.Chrome(executable_path = path)
     driver.get(url)
-    
 
-    #NO LONGER IN USE
-    '''CREATING CSV FILE DESTINATION'''
-    global EXECUTION_COUNT
-    FILE_TITLE="test_csv"+str(EXECUTION_COUNT)+'.csv'
-    #f=open(FILE_TITLE,'w')
-    '''^^if needed to make individual excel sheet for EACH GAME'''
-
-
-
-    #url = input('Enter - ')
-    #html = urlopen(url).read()
     html=driver.page_source
     soup = BeautifulSoup(html, "html.parser")
-    test_outer = soup.find('div', attrs={'id':"react-root",'class':"scorestream_ui"})
 
 
     '''
     CODE TO OBTAIN SCROLL CONTAINER + TAGS
     '''
 
-    #outer=soup.find('div', attrs={'class':'rmq-1c61845d'})
-    #print(outer)  
+    span_city_state=soup.find('span', attrs={'itemprop':'address'})
+    city_state=span_city_state.text
+    state=city_state[len(city_state)-2:]
+
+    
     scroll_container=soup.find('div', attrs={'class':'ReactVirtualized__Grid__innerScrollContainer'})
-    divs=scroll_container.find_all('div')
+    try:
+        a_tags=scroll_container.find_all('a')
+    except:
+        time.sleep(5)
+        html=driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        span_city_state=soup.find('span', attrs={'itemprop':'address'})
+        city_state=span_city_state.text
+        state=city_state[len(city_state)-2:]
+        scroll_container=soup.find('div', attrs={'class':'ReactVirtualized__Grid__innerScrollContainer'})
+        a_tags=scroll_container.find_all('a')
+    TEAM_NAME=str(soup.find('h1').text)
 
-    a_tags=scroll_container.find_all('a')
-    #print(len(a_tags))
-    #sys.exit()  
-
-
-    '''
-    TESTNG DIV RETRIEVAL INSIDE OF DIVS
-    '''
-
-
-    print("\n\nAUTOMATED SCORE COLLECTION \n\n")
-
-    i=0
-    cs_scores=[]
-
+    football_game=False
     for a in a_tags:
-        cs_score=""
-        first_div=(a.find('div'))
-        inner_div=first_div.find_all('div')
-        raw_sport=(str(get_spans(a)))
-        sport=('\n'+str(get_spans(a))+" Final Score:")
-        if sportKey in sport:
+        spans=a.find_all('span')
+        for span in spans:
+            if (sportKey in span.text):
+                football_game=True
+        div1=a.find('div')
+        div2=div1.find('div')
+        if (football_game):
             break
+
+    cancelled=False
+    inners= div2.find_all('div',attrs={'style':"font-size: 48px; display: flex; position: relative; flex-direction: column;"})
+    try: 
+        if ("Cancelled" in inners[0].find('div').text):
+            cancelled=True
+    except: pass
+    
+
+    ints=[]
+    final = False
+    for tag in inners[0].find_all('div'):
+        try:
+            ints.append(int(tag.text))
+        except:
+            pass
+        if(tag.text==('Final')):
+                final=True
+    
+    away_team=(spans[0].text+" "+spans[1].text)
+    home_team=(spans[2].text+" "+spans[3].text)
+
+    two_scores=True
+    date=(spans[7].text)
+    if ("\'18" in date and "\'19" not in date):
+        return TEAM_NAME+","+state+','+"N/A"+",no recent football scores available,"
+
+    if cancelled:
+        if(TEAM_NAME==(away_team)):
+            return TEAM_NAME+','+state+','+'@'+home_team+","+'Cancelled,'+datescratch.date_format(date)+','
+        else: return TEAM_NAME+','+state+','+'vs. '+away_team+','+'Cancelled,'+datescratch.date_format(date)+','
+
+    if 'Last' not in date and 'Yesterday' not in date and 'Today' not in date and football_game:
+        return TEAM_NAME+","+state+','+"N/A"+",most recent score isn't from this week"
+
+
+    if(len(ints)>2):
+        two_scores=False
+    else:
+        away_score=ints[0]
+        home_score=ints[1]
+
+    
+
+    RESULT = ""
+    outcome= ""
+    opponent=""
+    if TEAM_NAME==(away_team):
+        opponent="@ " + home_team
+        if(away_score>home_score):
+            outcome='W '+str(away_score)+"-"+str(home_score)
+        else: 
+            if(home_score>away_score):
+                outcome='L '+str(home_score)+"-"+str(away_score)
+        RESULT=away_team+','+state+','+'@ '+str(home_team)+','+outcome+','+datescratch.date_format(date)
+    else:
+        if TEAM_NAME==(home_team):
+            opponent="vs. " + away_team
+            if(away_score>home_score):
+                outcome='L '+str(away_score)+"-"+str(home_score)
+            else: 
+                if(home_score>away_score):
+                    outcome='W '+str(home_score)+"-"+str(away_score)
+        RESULT=home_team+','+state+','+'vs. '+str(away_team)+','+outcome+','+datescratch.date_format(date)
+    driver.close()
+    if not football_game:
+        return TEAM_NAME+","+state+','+"N/A"+",no recent football scores available,"
+    if not two_scores:
+        return(TEAM_NAME+","+state+','+opponent+",more than two score numbers pulled. check manually,")
+    if not final:
+        return(TEAM_NAME+","+state+','+opponent+",score available is not a final score,"+datescratch.date_format(date))
+    
+    
+    
+    return RESULT
+    
