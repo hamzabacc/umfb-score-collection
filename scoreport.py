5b9dc66d3b406eafc5e58a261a404852bafa9188
hamzabacc <46968192+hamzabacc@users.noreply.github.com>
2019-10-26 15:51:52 -0400

    working on second function

--- scoreport.py
+++ scoreport.py
@@ -1,10 +1,24 @@
 import scorescrape2
-#print(scorescrape2.getFullSchoolName('https://scorestream.com/team/pioneer-high-school-pioneers-8385'))
+print(scorescrape2.getScoreStream('https://scorestream.com/team/pioneer-high-school-pioneers-8385'))
 
-def d1(date):
-    date_csv = open(date+"_checklist.csv", "r")
-    for line in date_csv.readlines()[1:]:
-        pass
+def update_checklist(date):
+    date_csv_read = open(date+"_checklist.csv", "r")
+    data = date_csv_read.readlines()
+    date_csv_read.close()
+    date_csv = open(date+"_checklist.csv", "w")
+    date_csv.write("school name, state, done?, data, scorestream url\n")
+    team_info_pairs = {}
+    for line in data[1:]:
+        school_is_complete = line[2]
+        if school_is_complete=="Y":
+            date_csv.write(line)
+            continue
+        url = line[4][:-2]
+        return url
+        info = scorescrape2.getScoreStream(url)
+        final_score = info.split(",")[4]
+        if final_score.startswith("L ") or final_score.startwith("W "):
+            date_csv.write([line[0],line[1],'Y',info,url].join(','))
     return None
 
 
@@ -32,4 +46,4 @@ def date_checklist_creator(date):
     return None
 
 
-print(date_checklist_creator("october 26"))
+#print(date_checklist_creator("october 26"))
